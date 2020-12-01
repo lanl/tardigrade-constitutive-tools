30b24cb15b065f9b6d7ede262fb0a1f832a4c8a8
nathanm <nathanm@lanl.gov>
2020-12-01 09:26:55 -0700

    MAINT: Removed the hard-coded C++ standard

--- src/python/settings.py
+++ src/python/settings.py
@@ -1,7 +1,6 @@
 import os
 
 
-CMAKE_CXX_STANDARD = 11
 ROOT_DIRECTORY = os.path.abspath(os.path.join("..", ".."))
 PROJECT_CMAKE_FILE = os.path.join(ROOT_DIRECTORY, "CMakeLists.txt")
 PYTHON_SOURCE_SUBDIRECTORY = os.path.join("src", "python")
